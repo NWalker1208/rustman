902d20aea78dae669af3298e46b58ace5f4108fb
Nathan Walker <nathanwalker1208@gmail.com>
2021-12-27 17:30:52 -0800

    Modified to accept CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::process::exit;
+use std::env;
 
 fn main() {
-    match rustman::run("google-10000-english-usa-no-swears-medium.txt") {
+    let path = env::args().skip(1).next();
+    let path = path.unwrap_or(String::from("words.txt"));
+
+    match rustman::run(&path) {
         Err(e) => {
             println!("Error: {}", e);
             exit(1);
