e89dfde017e7250bceb8fa7b69571c14a5264622
Nathan Walker <nathanwalker1208@gmail.com>
2021-12-27 17:10:30 -0800

    Fixed bug, added larger word list

--- src/input.rs
+++ src/input.rs
@@ -4,14 +4,14 @@ use std::io::{stdout, Write};
 /// Gets a letter guess from the user.
 /// Forces them to choose a letter not present in the list of guessed characters.
 pub fn get_guess(already_guessed: &[char]) -> char {
-    let mut guess = String::new();
     loop {
         print!("Guess a letter: ");
         stdout().flush().expect("Could not flush stdout");
+        let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
-        match guess.trim().chars().next() {
+        match guess.to_lowercase().trim().chars().next() {
             Some(c) => match c {
                 'a'..='z' | 'A'..='Z' => {
                     if already_guessed.contains(&c) {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use std::process::exit;
 
 fn main() {
-    match rustman::run("words.txt") {
+    match rustman::run("google-10000-english-usa-no-swears-medium.txt") {
         Err(e) => {
             println!("Error: {}", e);
             exit(1);
-        },
+        }
         _ => (),
     }
 }
